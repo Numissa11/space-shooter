df3fa4748c7e17470ece69a6043f807b6d266a3f
Aline <aline_sinclaire@hotmail.fr>
2020-12-30 00:41:20 +0100

    key press reacting

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
- 
-$(
-      function() {
-          console.log('hello from main.js ready()');
-      }
-  );
+$(function () {
+    $(document).keydown(
+        function (e) {
+            switch (e.which) {
+                case GameSettings.keyPress.up:
+                    console.log('up');
+                    break;
+                case GameSettings.keyPress.down:
+                    console.log('down');
+                    break;
+                case GameSettings.keyPress.left:
+                    console.log('left');
+                    break;
+                case GameSettings.keyPress.right:
+                    console.log('right');
+                    break;
+                case GameSettings.keyPress.space:
+                    console.log('space');
+                    break;
+            }
+        }
+    );
+});
