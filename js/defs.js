fa08308eb21f57fd285017037ea13cd4a562b88e
Aline <aline_sinclaire@hotmail.fr>
2020-12-30 02:26:57 +0100

    adding images and processing the assets but need the comments

--- js/defs.js
+++ js/defs.js
@@ -1,9 +1,29 @@
-const GameSettings = {
+const ImageFiles = [
+      'playerShip1_blue'
+  ];
+  
+  const GameSettings = {
       keyPress: {
           left: 37,
           right: 39,
           up: 38,
           down: 40,
           space: 32
+      },
+      playAreaWidth: 800,
+      playAreaHeight: 600,
+      playAreaDiv: '#playArea',
+  
+      playerDivName: 'playerSprite',
+      playerStart: {
+          x: 440,
+          y: 360
       }
   };
+  
+  let GameManager = {
+      assets : {},
+      player: undefined
+  };
+  
+  
--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
+function init() {
+    console.log('Main Game init()');
+}
+
+function processAsset(indexNum) {
+    let img = new Image();
+    let fileName = `assets/ ${ImageFiles[indexNum]}.png`;
+    img.src = fileName;
+    img.onload = function() {
+        GameManager.assets[ImageFiles[indexNum]] = {
+            width: this.width,
+            height: this.height,
+            fileName: fileName
+        };
+        indexNum++;
+        if (indexNum < ImageFiles.length) {
+            processAsset(indexNum);
+        } else {
+            console.log('Assets Done:', GameManager.assets);
+            init();
+        }
+    }
+}
+
 $(function () {
+    processAsset(0);
     $(document).keydown(
         function (e) {
             switch (e.which) {
