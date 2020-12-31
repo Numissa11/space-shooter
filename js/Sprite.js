53b8a3bfe98a7001acafc6ecdb0ac7ad68613f1e
Aline <aline_sinclaire@hotmail.fr>
2020-12-31 16:15:44 +0100

    function draw and removeFromBoard done, using jquery selector

--- js/Sprite.js
+++ js/Sprite.js
@@ -4,21 +4,35 @@ class Sprite {
             this.position = position;
             this.imgName = imgName;
             this.size = sizePx;
-            this.anchorShift = new Point(-this.size.width / 2, -this.size.height /2);
+            this.anchorShift = new Point(-this.size.width / 2, -this.size.height / 2);
+      }
+
+      removeFromBoard() {
+            $('#' + this.divName).remove();
+      }
+
+      draw() {
+            $('#' + this.divName).css({
+                  'left': this.position.x,
+                  'top': this.position.y
+            });
       }
 
       setPosition(x, y, shift) {
             this.position.update(x, y);
-            if (shift == true){
+            if (shift == true) {
                   this.incrementPosition(this.anchorShift.x, this.anchorShift.y);
             }
+            this.draw();
       }
 
       updatePosition(x, y) {
-            this.position.update(x, y)
+            this.position.update(x, y);
+            this.draw();
       }
 
       incrementPosition(ix, iy) {
-            this.position.increment(ix, iy)
+            this.position.increment(ix, iy);
+            this.draw();
       }
 }
